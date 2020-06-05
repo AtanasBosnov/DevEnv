a90215c82431bf58d48cdf1b4bff42ffc78c9b5f
Atanas Bosnov <abosnov@gmail.com>
2020-06-05 15:21:00 +0300

    Adding package.json file to the project

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require("http");
+const fs = require("fs"); //file handling
 const port = 3000;
+//import { bootsrap } from "../DevEnv/css";
 
 const server = http.createServer(function(req, res) {
-  res.write("Hello from server");
-  res.end();
+  //telling the browser that we send an HTML file so it parces it like HTML
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", function(error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("File not found!");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(port, function(error) {

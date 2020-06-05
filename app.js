9c8066ea189457a827e78a00cc3c51854ccbb888
Atanas Bosnov <abosnov@gmail.com>
2020-06-05 14:12:04 +0300

    Node server set up and running

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("WOOOOOW!");
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+  res.write("Hello from server");
+  res.end();
+});
+
+server.listen(port, function(error) {
+  if (error) {
+    console.log("Something went wrong...");
+  } else {
+    console.log("Server is listening on port " + port);
+  }
+});
